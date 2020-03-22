3955e6ed0ba54dd79eb41144b6a535460053b41b
sebastientromp <sebastien.tromp@gmail.com>
2020-03-22 14:41:52 +0100

    UI: add support for multi opponents in BGS next opponent panel

--- core/src/js/components/battlegrounds/in-game/bgs-next-opponent-overview.ts
+++ core/src/js/components/battlegrounds/in-game/bgs-next-opponent-overview.ts
@@ -1,11 +1,13 @@
 import {
 	AfterViewInit,
 	ChangeDetectionStrategy,
+	ChangeDetectorRef,
 	Component,
 	ElementRef,
 	HostListener,
 	Input,
 	Renderer2,
+	ViewRef,
 } from '@angular/core';
 import { Entity } from '@firestone-hs/replay-parser';
 import { BgsGame } from '../../../models/battlegrounds/bgs-game';
@@ -21,6 +23,7 @@ declare var amplitude: any;
 	styleUrls: [
 		`../../../../css/global/components-global.scss`,
 		`../../../../css/component/battlegrounds/in-game/bgs-next-opponent-overview.component.scss`,
+		`../../../../css/global/scrollbar.scss`,
 	],
 	template: `
 		<div class="container">
@@ -28,24 +31,24 @@ declare var amplitude: any;
 				<bgs-hero-face-off *ngFor="let faceOff of opponentFaceOffs" [faceOff]="faceOff"></bgs-hero-face-off>
 			</div>
 			<div class="content">
-				<div class="next-opponent-overview">
-					<div class="header">
+				<div class="next-opponent-overview" *ngFor="let opponentInfo of opponentInfos">
+					<div class="header" (click)="toggleDisplayBody(opponentInfo)">
 						<div class="portrait">
-							<img [src]="icon" class="icon" />
-							<div class="name">{{ name }}</div>
+							<img [src]="opponentInfo.icon" class="icon" />
+							<div class="name">{{ opponentInfo.name }}</div>
 							<!-- <img [src]="taverTierIcon" class="tavern-tier" /> -->
-							<div class="tavern-tier">Tavern: {{ tavernTier }}</div>
+							<div class="tavern-tier">Tavern: {{ opponentInfo.tavernTier }}</div>
 						</div>
-						<board [entities]="boardMinions" *ngIf="boardMinions"></board>
+						<board [entities]="opponentInfo.boardMinions" *ngIf="opponentInfo.boardMinions"></board>
 					</div>
-					<div class="body">
+					<div class="body" *ngIf="opponentInfo.displayBody">
 						<div class="tavern-upgrades">
-							<div *ngFor="let upgrade of tavernUpgrades">
+							<div *ngFor="let upgrade of opponentInfo.tavernUpgrades">
 								Turn {{ upgrade.turn }}: Upgrade tier {{ upgrade.tavernTier }}
 							</div>
 						</div>
 						<div class="triple-tiers">
-							<div *ngFor="let triple of triples">
+							<div *ngFor="let triple of opponentInfo.triples">
 								Turn {{ triple.turn }}: One tier {{ triple.tierOfTripledMinion }} triple
 							</div>
 						</div>
@@ -57,12 +60,7 @@ declare var amplitude: any;
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BgsNextOpponentOverviewComponent implements AfterViewInit {
-	icon: string;
-	name: string;
-	tavernTier: string;
-	boardMinions: readonly Entity[];
-	tavernUpgrades: readonly BgsTavernUpgrade[];
-	triples: readonly BgsTriple[];
+	opponentInfos: readonly OpponentInfo[] = [];
 	opponentFaceOffs: readonly OpponentFaceOff[];
 
 	private _panel: BgsNextOpponentOverviewPanel;
@@ -70,7 +68,6 @@ export class BgsNextOpponentOverviewComponent implements AfterViewInit {
 
 	@Input() set panel(value: BgsNextOpponentOverviewPanel) {
 		this._panel = value;
-		this.icon = `https://static.zerotoheroes.com/hearthstone/cardart/256x/${value.opponentOverview.cardId}.jpg`;
 		this.updateInfo();
 	}
 
@@ -79,29 +76,42 @@ export class BgsNextOpponentOverviewComponent implements AfterViewInit {
 		this.updateInfo();
 	}
 
-	constructor(private readonly el: ElementRef, private readonly renderer: Renderer2) {}
+	constructor(
+		private readonly el: ElementRef,
+		private readonly renderer: Renderer2,
+		private readonly cdr: ChangeDetectorRef,
+	) {}
 
 	async ngAfterViewInit() {
 		this.onResize();
 	}
 
+	toggleDisplayBody(opponentInfo: OpponentInfo) {
+		opponentInfo.displayBody = !opponentInfo.displayBody;
+		if (!(this.cdr as ViewRef).destroyed) {
+			this.cdr.detectChanges();
+		}
+	}
+
 	@HostListener('window:resize')
 	onResize() {
-		const boardContainer = this.el.nativeElement.querySelector('board');
-		const rect = boardContainer.getBoundingClientRect();
-		// console.log('boardContainer', boardContainer, rect);
-		// const constrainedByWidth = rect.width <
-		const cardElements = boardContainer.querySelectorAll('li');
-		// console.log('cardElements', cardElements);
-		let cardWidth = rect.width / 8;
-		let cardHeight = 1.48 * cardWidth;
-		if (cardHeight > rect.height) {
-			cardHeight = rect.height;
-			cardWidth = cardHeight / 1.48;
-		}
-		for (const cardElement of cardElements) {
-			this.renderer.setStyle(cardElement, 'width', cardWidth + 'px');
-			this.renderer.setStyle(cardElement, 'height', cardHeight + 'px');
+		const boardContainers = this.el.nativeElement.querySelectorAll('board');
+		for (const boardContainer of boardContainers) {
+			const rect = boardContainer.getBoundingClientRect();
+			// console.log('boardContainer', boardContainer, rect);
+			// const constrainedByWidth = rect.width <
+			const cardElements = boardContainer.querySelectorAll('li');
+			// console.log('cardElements', cardElements);
+			let cardWidth = rect.width / 8;
+			let cardHeight = 1.48 * cardWidth;
+			if (cardHeight > rect.height) {
+				cardHeight = rect.height;
+				cardWidth = cardHeight / 1.48;
+			}
+			for (const cardElement of cardElements) {
+				this.renderer.setStyle(cardElement, 'width', cardWidth + 'px');
+				this.renderer.setStyle(cardElement, 'height', cardHeight + 'px');
+			}
 		}
 	}
 
@@ -109,19 +119,33 @@ export class BgsNextOpponentOverviewComponent implements AfterViewInit {
 		if (!this._panel || !this._game) {
 			return;
 		}
-		const opponent = this._game.players.find(player => player.cardId === this._panel.opponentOverview.cardId);
-		console.log('found opponent', opponent, this._panel, this._game);
-		if (!opponent) {
+		const nextOpponent = this._game.players.find(player => player.cardId === this._panel.opponentOverview.cardId);
+		if (!nextOpponent) {
 			return;
 		}
-		this.name = opponent.name;
-		this.tavernTier = '' + opponent.getCurrentTavernTier();
-		this.boardMinions = opponent.getLastKnownBoardState();
-		console.log('boardMinions', JSON.stringify(this.boardMinions, null, 4));
-		this.tavernUpgrades = [...opponent.tavernUpgradeHistory].reverse();
-		this.triples = [...opponent.tripleHistory].reverse();
-
 		const opponents = this._game.players.filter(player => !player.isMainPlayer);
+		this.opponentInfos = opponents
+			.map(
+				opponent =>
+					({
+						id: opponent.cardId,
+						icon: `https://static.zerotoheroes.com/hearthstone/cardart/256x/${opponent.cardId}.jpg`,
+						name: opponent.name,
+						tavernTier: '' + opponent.getCurrentTavernTier(),
+						boardMinions: opponent.getLastKnownBoardState(),
+						tavernUpgrades: [...opponent.tavernUpgradeHistory].reverse(),
+						triples: [...opponent.tripleHistory].reverse(),
+						displayBody: opponent.cardId === this._panel.opponentOverview.cardId,
+					} as OpponentInfo),
+			)
+			.sort((a, b) =>
+				a.id === this._panel.opponentOverview.cardId
+					? -1
+					: b.id === this._panel.opponentOverview.cardId
+					? 1
+					: 0,
+			);
+
 		this.opponentFaceOffs = opponents.map(
 			opp =>
 				({
@@ -137,4 +161,15 @@ export class BgsNextOpponentOverviewComponent implements AfterViewInit {
 				} as OpponentFaceOff),
 		);
 	}
+}
+
+interface OpponentInfo {
+	id: string;
+	icon: string;
+	name: string;
+	tavernTier: string;
+	boardMinions: readonly Entity[];
+	tavernUpgrades: readonly BgsTavernUpgrade[];
+	triples: readonly BgsTriple[];
+	displayBody: boolean;
 }
